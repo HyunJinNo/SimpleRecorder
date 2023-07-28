8e456a6f5820aa1edcde098d84d5c26614a9f257
HyunJinNo <nhj9831@naver.com>
2023-07-28 16:47:59 +0900

    녹음된 파일을 외부 저장소에 저장하는 기능 구현

--- app/src/main/java/com/example/simplerecorder/RecordingActivity.kt
+++ app/src/main/java/com/example/simplerecorder/RecordingActivity.kt
@@ -6,23 +6,31 @@ import android.media.MediaPlayer
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.simplerecorder.databinding.ActivityRecordingBinding
+import java.io.File
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class RecordingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecordingBinding
     private var recorder: MediaRecorder? = null
     private val recordingFilePath: String by lazy {
-        "${externalCacheDir?.absolutePath}/recording.3gp"
+        "${Environment.getExternalStorageDirectory().absolutePath}/recording.3gp"
     }
     private var player: MediaPlayer? = null
     private var filename = ""
 
     // Requesting permission to RECORD_AUDIO
     private var permissionToRecordAccepted = false
-    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissions: Array<String> = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     companion object {
         private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
@@ -53,6 +61,9 @@ class RecordingActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = AudioAdapter(arrayOf<String>("A", "B", "C"))
         binding.recyclerView.adapter = adapter
+
+        initListeners()
+        requestPermissions(permissions, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     override fun onDestroy() {
@@ -89,11 +100,21 @@ class RecordingActivity : AppCompatActivity() {
         player = null
     }
 
-    fun startRecording() {
+    private fun startRecording() {
+        val sdCard = Environment.getExternalStorageDirectory()
+
+        val tempPath = sdCard.absolutePath + "/Simple Recorder"
+        if (!File(tempPath).exists()) {
+            File(tempPath).mkdirs()
+        }
+
+        val file = File(sdCard, "Simple Recorder/recorded.mp4")
+        val filepath = file.absolutePath
+
         recorder = MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC) // 오디오 소스 설정 (마이크)
             setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP) // 출력 파일 포맷 설정
-            setOutputFile(recordingFilePath) // 출력 파일 이름 설정
+            setOutputFile(filepath) // 출력 파일 이름 설정
             setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
 
             try {
@@ -117,6 +138,7 @@ class RecordingActivity : AppCompatActivity() {
     private fun stopRecording() {
         recorder?.apply {
             stop()
+            reset()
             release()
         }
         recorder = null
